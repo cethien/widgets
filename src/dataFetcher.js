6cbced6b32e79018ed2e7d74180e4deced9009b5
cethien <borislaw.sotnikow@gmx.de>
2023-01-07 15:27:57 +0100

    feature: added hiding mechanic

--- src/dataFetcher.js
+++ src/dataFetcher.js
@@ -1,9 +1,20 @@
 'use strict';
 
+const tunaServer = 'http://localhost:1608';
 const titleRegex = /\((feat.|ft.|featuring|with|from)(.*)\)/i;
 
+export async function isTunaServerReachable() {
+	try {
+		var res = await fetch(tunaServer);
+		var status = await res.status;
+		return (status >= 200 && status < 300) || status === 304;
+	} catch (error) {
+		return false;
+	}
+}
+
 export async function getData() {
-	var res = await fetch('http://localhost:1608');
+	var res = await fetch(tunaServer);
 	var data = await res.json();
 	return {
 		status: data.status,
--- src/main.js
+++ src/main.js
@@ -1,30 +1,36 @@
 'use strict';
 
+var app = document.getElementById('app');
 var artists = document.getElementById('artists');
 var title = document.getElementById('title');
 
 import * as fetcher from './dataFetcher.js';
 
-async function setData() {
-	var data = await fetcher.getData();
-	if (data.status === 'playing') {
-		artists.innerText = data.artists.join(', ');
-		title.innerText = data.title;
-	}
+async function setData(data) {
+	artists.innerText = data.artists.join(', ');
+	title.innerText = data.title;
 }
 
 async function update() {
-	try {
-		await setData();
-	} catch (err) {
-		console.log(err);
+	var data = await fetcher.getData();
+	var appVisibility = data.status === 'playing' ? 'visible' : 'hidden';
+	if (data.status === 'playing') {
+		await setData(data);
 	}
+	app.style.visibility = appVisibility;
 }
 
 export async function run() {
 	try {
-		// update every 1 second
-		setInterval(update, 1000);
+		var isServerOnline = await fetcher.isTunaServerReachable();
+		if (isServerOnline) {
+			// update every 1 second
+			setInterval(update, 1000);
+		} else {
+			console.log(
+				"Tuna Server couldn't be reached. Is the Tuna Server running on http://localhost:1608?"
+			);
+		}
 	} catch (error) {
 		console.error(error);
 	}
